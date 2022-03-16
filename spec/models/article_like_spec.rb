998e2e7943919b7e23cd33320188e6cd3edfd46f
s0214m <m.suguru.0214@gmail.com>
2022-03-16 16:56:12 +0900

    article_like modelのテスト実装

--- spec/models/article_like_spec.rb
+++ spec/models/article_like_spec.rb
@@ -1,25 +1,41 @@
-# == Schema Information
-#
 # Table name: article_likes
-#
 #  id         :bigint           not null, primary key
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  article_id :bigint           not null
 #  user_id    :bigint           not null
-#
-# Indexes
-#
+# Indexex
 #  index_article_likes_on_article_id  (article_id)
 #  index_article_likes_on_user_id     (user_id)
-#
 # Foreign Keys
-#
 #  fk_rails_...  (article_id => articles.id)
 #  fk_rails_...  (user_id => users.id)
-#
+
 require "rails_helper"
 
 RSpec.describe ArticleLike, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "userとarticleが存在する場合" do
+    it "いいねが付く" do
+      article_like = build(:article_like)
+      expect(article_like).to be_valid
+    end
+  end
+
+  context "userが存在しない場合" do
+    it "いいねがつかない" do
+      article_like = build(:article_like, user: nil)
+      expect(article_like).to be_invalid
+      res = article_like.errors.details[:user][0][:error]
+      expect(res).to eq :blank
+    end
+  end
+
+  context "articleが存在しない場合" do
+    it "いいねが付かない" do
+      article_like = build(:article_like, article: nil)
+      expect(article_like).to be_invalid
+      res = article_like.errors.details[:article][0][:error]
+      expect(res).to eq :blank
+    end
+  end
 end
