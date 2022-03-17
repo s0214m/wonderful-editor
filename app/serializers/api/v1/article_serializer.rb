263194bd91648e4655ed7b2bfae712d57714d69e
s0214m <m.suguru.0214@gmail.com>
2022-03-17 20:38:50 +0900

    show methodとそのテストの実装

---
+++ app/serializers/api/v1/article_serializer.rb
@@ -0,0 +1,8 @@
+class Api::V1::ArticleSerializer < ActiveModel::Serializer
+  attributes :id, :title, :body, :updated_at
+
+  belongs_to :user, serializer: Api::V1::UserSerializer
+
+  has_many :comments
+  has_many :article_likes
+end
