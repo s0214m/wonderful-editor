a55e7cac5f87d35fda29c335fab18563cbaccd7d
s0214m <m.suguru.0214@gmail.com>
2022-03-17 13:26:00 +0900

    Apiのルーティングの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for "User", at: "auth"
-
+  namespace :api, format: "json" do
+    namespace :v1 do
+      mount_devise_token_auth_for "User", at: "auth"
+      resources :article
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
