c1ad04de01adf30770edc76d96cec2ceab497764
s0214m <m.suguru.0214@gmail.com>
2022-03-19 09:22:06 +0900

    rails db:migrate:resetを実行

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,14 +1,22 @@
+# == Schema Information
+#
 # Table name: articles
+#
 #  id         :bigint           not null, primary key
-#  body       :text                       presence: true
-#  title      :string                     presence: ture
+#  body       :text
+#  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  user_id    :bigint           not null  user must be exist
+#  user_id    :bigint           not null
+#
 # Indexes
+#
 #  index_articles_on_user_id  (user_id)
+#
 # Foreign Keys
+#
 #  fk_rails_...  (user_id => users.id)
+#
 
 require "rails_helper"
 
